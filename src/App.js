9843183217d93c4cf6e22afca38f7d5704c95cde
lesley <lesleybanadzem2017@gmail.com>
2019-08-19 18:51:58 -0700

    Made timer to start counting up in minutes and seconds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import { CombineLatestOperator } from "rxjs/internal/observable/combineLatest";
+import { SourceNode } from "../../../Library/Caches/typescript/3.5/node_modules/source-map/source-map";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-const [homeScore, setHomeScore] = useState(0);
-const [awayScore, setAwayScore] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
 
+  const [seconds, setSecondsTime] = useState(0)
+  const [minutes, setMinuteTime] = useState(0);
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setSecondsTime(seconds + 1)
+      if(seconds >= 59){
+        setSecondsTime(0)
+        setMinuteTime(minutes + 1)
+      }
+    }, 1000);
+    return () => clearInterval(interval)
+  });
+
+
+  
   return (
     <div className="container">
       <section className="scoreboard">
@@ -19,7 +36,7 @@ const [awayScore, setAwayScore] = useState(0);
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer" >{minutes}:{seconds}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
