a9899710574eb8f5f99328b49a675fcc912e5168
lesley <lesleybanadzem2017@gmail.com>
2019-08-19 17:28:22 -0700

    added onclick to update scores on click

--- src/App.js
+++ src/App.js
@@ -30,12 +30,24 @@ const [awayScore, setAwayScore] = useState(0);
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick ={e=> {
+            e.preventDefault();
+            setHomeScore(homeScore + 7)
+          }}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal"onClick ={e=> {
+            e.preventDefault();
+            setHomeScore(homeScore + 3)
+          }} >Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown"  onClick ={e=> {
+            e.preventDefault();
+            setAwayScore(awayScore+ 7)
+          }}>>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal"  onClick ={e=> {
+            e.preventDefault();
+            setAwayScore(awayScore + 3)
+          }}>>Away Field Goal</button>
         </div>
       </section>
     </div>
