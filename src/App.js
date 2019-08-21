72c90062fff4197d18a2a7bfef57f29c915b85a9
lesley <lesleybanadzem2017@gmail.com>
2019-08-21 13:45:24 -0700

    Created a Score Component and imported it in to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+// import BottomRow from "./BottomRow";
+import ScoreBoard from './components/ScoreBoard'
 import { CombineLatestOperator } from "rxjs/internal/observable/combineLatest";
 import { SourceNode } from "../../../Library/Caches/typescript/3.5/node_modules/source-map/source-map";
 
@@ -12,6 +13,7 @@ function App() {
 
   const [seconds, setSecondsTime] = useState(0)
   const [minutes, setMinuteTime] = useState(0);
+  //Use Effect for To Start timer
   useEffect(()=>{
     const interval = setInterval(()=>{
       setSecondsTime(seconds + 1)
@@ -27,23 +29,15 @@ function App() {
   
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer" >{minutes}:{seconds}</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+      <ScoreBoard 
+        //passinbg in props into the scoreboard comnponent  
+        hScore = {homeScore}
+        min = {minutes}
+        secs = {seconds}
+        aScore = {awayScore}
+      />
+    
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
@@ -51,6 +45,7 @@ function App() {
             e.preventDefault();
             setHomeScore(homeScore + 7)
           }}>Home Touchdown</button>
+       
           <button className="homeButtons__fieldGoal"onClick ={e=> {
             e.preventDefault();
             setHomeScore(homeScore + 3)
